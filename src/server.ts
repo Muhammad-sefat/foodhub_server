fd75ac6ab811e01d1cf301dc4ef64ae384425026
Muhammad Sefat <muhammadsefat55@gmail.com>
2026-01-28 22:23:37 +0600

    some changes

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,17 @@
 import app from "./app.js";
 import { env } from "./config/env.js";
+import { prisma } from "./lib/prisma.js";
 
 async function main() {
   try {
-    // await prisma.$connect();
-    // console.log("Connent to the database successfully");
+    await prisma.$connect();
+    console.log("Connent to the database successfully");
     app.listen(env.PORT, () => {
       console.log(`Server is running on http://localhost:${env.PORT}`);
     });
   } catch (error) {
-    // console.log("An error occurd", error);
-    // await prisma.$disconnect();
+    console.log("An error occurd", error);
+    await prisma.$disconnect();
     process.exit(1);
   }
 }
