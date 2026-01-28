b53828ffcc02624d606addb890f6b59697278a01
Muhammad Sefat <muhammadsefat55@gmail.com>
2026-01-28 23:20:23 +0600

    successfully connect with prisma and working on connecting better auth

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,14 @@
 import express, { Application } from "express";
 import cors from "cors";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth";
 
 const app: Application = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.all("/api/auth/*splat", toNodeHandler(auth));
 
 app.get("/", (_req, res) => {
   res.send("FoodHub API is running 🚀");
